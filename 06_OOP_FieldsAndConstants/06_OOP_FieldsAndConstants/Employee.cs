e4c4e82f450ab08776b1260dcfbf7deb58cf1d73
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-24 02:55:31 +0300

    OOP - Fields & Constants

---
+++ 06_OOP_FieldsAndConstants/06_OOP_FieldsAndConstants/Employee.cs
@@ -0,0 +1,39 @@
+﻿namespace _06_OOP_FieldsAndConstants
+{
+    // <class access-modifier> class <class-name> { class block }
+    // <class access-modifier> class <class-name> : <base-class> { }
+    // <class access-modifier> class <class-name> : <interface-name> { }
+    // <class access-modifier> class <class-name> : <base-class>, <interface-name> { }
+    // <class access-modifier> class <class-name> : <base-class>, <interface-name>, <interface-name> { }
+    // <class access-modifier> class <class-name> : <base-class>, <interface-name>, <interface-name>, <interface-name> { }
+
+    // <class access-modifier> => public, internal (default), private, protected, protected internal, private protected
+    // public => Accessible from any other class
+    // internal => Accessible only from the same assembly
+    // private => Accessible only from the same class
+    // protected => Accessible only from the same class and derived
+    // protected internal => Only code in the same assembly or in a derived class in another assembly can access this type or member
+    // private protected => Only code in the same assembly and in the same class or a derived class can access the type or member
+    public class Employee
+    {
+        // Constants => A constant is a field that can't be changed after the object is created
+        // Constants => <access modifier> const <data type> <field name> = <value>;
+        // Constants => const <data type> <field name> = <value>; (default access modifier is private)
+        // Constants are accessed through the class name, not through an object
+        public const double TAX = 0.03;
+
+        // Field => <access modifier> <data type> <field name> = <initial value>;
+        // Field => <access modifier> <data type> <field name>;
+        // Field => <data type> <field name> = <initial value>; (default access modifier is private)
+        // Field => <data type> <field name>; (default access modifier is private)
+        // Field => <access modifier> const <data type> <field name> = <initial value>;
+        // Field => <access modifier> readonly <data type> <field name> = <initial value>;
+        // Field => <data type> <field name> = <initial value>; (default access modifier is private)
+        // Field => <data type> <field name>; (default access modifier is private)
+        // Fields are accessed through an object of the class
+        public string FName;
+        public string LName;
+        public double Wage;
+        public double LoggedHours;
+    }
+}
---
+++ 06_OOP_FieldsAndConstants/06_OOP_FieldsAndConstants/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _06_OOP_FieldsAndConstants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Object (Instance) Creation Syntax: 
+            // Declaration: <class-name / type> <object-name>;
+            // Assignment: <object-name> = new <class-name / type>();
+
+            // Initialization: <class-name / type> <object-name> = new <class-name / type>();
+
+            Employee[] employees = new Employee[2];
+
+            Employee e1 = new Employee();
+
+            Console.WriteLine("First Employee: ");
+            Console.Write("First Name: ");
+            e1.FName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            e1.LName = Console.ReadLine();
+
+            Console.Write("Wage: ");
+            e1.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Logged Hours: ");
+            e1.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            employees[0] = e1;
+
+            Employee e2 = new Employee();
+
+            Console.WriteLine("\nSecond Employee: ");
+            Console.Write("First Name: ");
+            e2.FName = Console.ReadLine();
+
+            Console.Write("Last Name: ");
+            e2.LName = Console.ReadLine();
+
+            Console.Write("Wage: ");
+            e2.Wage = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Logged Hours: ");
+            e2.LoggedHours = Convert.ToDouble(Console.ReadLine());
+
+            employees[1] = e2;
+
+            foreach (var emp in employees)
+            {
+                var netSalary = emp.Wage * emp.LoggedHours * (1 - Employee.TAX);
+
+                Console.WriteLine("--------------------");
+                Console.WriteLine($"Employee: {emp.FName} {emp.LName}");
+                Console.WriteLine($"Wage: {emp.Wage}");
+                Console.WriteLine($"Logged Hours: {emp.LoggedHours}");
+                Console.WriteLine($"Net Salary: {netSalary}");
+                Console.WriteLine("--------------------");
+            }
+        }
+    }
+}
