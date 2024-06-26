17d73d1742d908a956a4c276f548de9b821a3be6
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-26 17:10:04 +0300

    OOP - Events

---
+++ 12_OOP_Events/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _12_OOP_Events
+{
+    // Subscriber
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Events enable a class or object to notify other classes or objects when something of interest occurs.
+            // The class that sends (or raises) the event is called the publisher and the classes that receive (or handle) the event are called subscribers.
+            // Events are a type of multicast delegate, which means that they can hold references to more than one event handler.
+            // The publisher determines when an event is raised; the subscribers determine what action is taken in response to the event.
+            // Events are declared using delegates.
+
+            Stock stock = new Stock("Apple");
+            stock.Price = 100;
+
+            // Subscribe to the event 
+            stock.OnPriceChanged += Stock_OnPriceChanged;
+
+            // Another way to subscribe to the event
+            //stock.OnPriceChanged += (Stock stock, decimal oldPrice) =>
+            //{
+            //    if (stock.Price > oldPrice)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Green;
+            //    }
+            //    else if (stock.Price < oldPrice)
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //    }
+            //    else
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Gray;
+            //    }
+
+            //    Console.WriteLine($"{stock.Name}: {stock.Price}$");
+            //};
+
+            stock.ChangePriceByPercent(0.05m);
+            stock.ChangePriceByPercent(-0.03m);
+            stock.ChangePriceByPercent(0.00m);
+
+            // Unsubscribe from the event
+            stock.OnPriceChanged -= Stock_OnPriceChanged;
+
+            stock.ChangePriceByPercent(0.10m);
+
+        }
+
+        // Method that will be called when the event is raised
+        private static void Stock_OnPriceChanged(Stock stock, decimal oldPrice)
+        {
+            if (stock.Price > oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else if (stock.Price < oldPrice)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+
+            Console.WriteLine($"{stock.Name}: {stock.Price}$");
+        }
+    }
+}
---
+++ 12_OOP_Events/Stock.cs
@@ -0,0 +1,34 @@
+﻿namespace _12_OOP_Events
+{
+    // Declaration of the delegate
+    public delegate void StockPriceChangeHandler(Stock stock, decimal oldPrice);
+
+    // Publisher
+    public class Stock
+    {
+        private string _name;
+        private decimal _price;
+
+        public string Name => _name;
+        public decimal Price { get => _price; set => _price = value; }
+
+        // Event declaration using the delegate 
+        public event StockPriceChangeHandler OnPriceChanged;
+
+        public Stock(string stockName)
+        {
+            _name = stockName;
+        }
+
+        public void ChangePriceByPercent(decimal percent)
+        {
+            decimal oldPrice = _price;
+            _price += Math.Round(_price * percent, 2);
+
+            if (OnPriceChanged is not null)         // ensure that there are subscribers
+            {
+                OnPriceChanged(this, oldPrice);     // raise the event (call the delegate) / notify the subscribers / publish the event
+            }
+        }
+    }
+}
