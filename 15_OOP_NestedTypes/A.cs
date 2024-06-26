6392208255b1071705b9e40ba32049ac3185b83c
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-26 19:34:21 +0300

    OOP - Nested Types

---
+++ 15_OOP_NestedTypes/A.cs
@@ -0,0 +1,24 @@
+﻿namespace _15_OOP_NestedTypes
+{
+    // Containing Type
+    class A
+    {
+        private int _x;
+        // Nested Type is a type defined within another type.
+        // Nested types can be public, private, protected, or internal.
+        // Nested types doesn't pollute the global namespace.
+        // Nested types can access private members of the containing type.
+        // Nested types can be generic.
+
+        // Nested Type
+        // We use nested types when we want to encapsulate a type that is only used by the containing type. 
+        class B
+        {
+            void SetX(int x)
+            {
+                A a = new A();
+                a._x = x;       // Accessing private member of containing type.
+            }
+        }
+    }
+}
---
+++ 15_OOP_NestedTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _15_OOP_NestedTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.WriteLine(employee.EmployeeInsurance?.CompanyName);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Composition - Nested Type => is a type that is defined within another type
+        // Composition is a strong relationship between two classes where one contains the other and the contained class cannot exist without the container class.
+        // Composition is a "has a" relationship.
+        public Insurance EmployeeInsurance { get; set; }
+
+        public Employee()
+        {
+            EmployeeInsurance = new Insurance() { PolicyId = -1, CompanyName = "N/A" };
+        }
+
+        // Composition - Nested Type => is a type that is defined within another type
+        public class Insurance
+        {
+            public int PolicyId { get; set; }
+            public string CompanyName { get; set; }
+        }
+    }
+
+    class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
