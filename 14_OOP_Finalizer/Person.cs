2dabd1b606072a39130ae19f0693296462380b6c
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-06-26 18:26:45 +0300

    OOP - Finalizer

---
+++ 14_OOP_Finalizer/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace _14_OOP_Finalizer
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        // Constructor of the class is called when the object of the class is created and memory is allocated for the object.
+        public Person()
+        {
+            Console.WriteLine("This is person constructor");
+        }
+
+        // Destructor of the class is called when the object of the class is destroyed and memory is deallocated for the object.
+        // Object is destroyed when the program ends or when the object is no longer needed or when the object is explicitly destroyed or when the object goes out of scope or when the object is set to null.
+        ~Person()
+        {
+            Console.WriteLine("This is person destructor");
+        }
+
+    }
+}
---
+++ 14_OOP_Finalizer/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _14_OOP_Finalizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Mohamed";
+
+
+            // Grabage collector is used to manage the memory and to deallocate the memory for the objects that are no longer needed.
+            // Garbage collector is a part of CLR (Common Language Runtime). 
+            // Garbage collector runs in the background and it is responsible for deallocating the memory for the objects that are no longer needed.
+            // Garbage collector uses a technique called "Mark and Sweep" to deallocate the memory for the objects that are no longer needed.
+            // Garbage collector is non-deterministic. It runs in the background and it is not possible to predict when the garbage collector will run.
+            // Garbage collector is not available in C++ and C. In C++ and C, the programmer is responsible for deallocating the memory for the objects that are no longer needed.
+            // Garbage collector is available in C# and Java. In C# and Java, the garbage collector is responsible for deallocating the memory for the objects that are no longer needed.
+
+            MakeSomeGarbage();
+
+            Console.WriteLine($"Memory used before collection: {GC.GetTotalMemory(false):N0}");
+            GC.Collect();   // Explicitly calling the garbage collector to deallocate the memory for the objects that are no longer needed.
+            Console.WriteLine($"Memory used after collection: {GC.GetTotalMemory(true):N0}");
+
+            Console.ReadKey();
+        }
+
+        static void MakeSomeGarbage()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                var person = new Person();
+            }
+        }
+    }
+}
