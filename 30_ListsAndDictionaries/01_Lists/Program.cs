7c1c884dee61425385d1edf316496a2e56354637
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-07-01 15:20:42 +0300

    Lists and Dictionaries

---
+++ 30_ListsAndDictionaries/01_Lists/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace _01_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lists are similar to arrays, but they can grow and shrink in size dynamically. 
+            // They are also more flexible than arrays.
+            // Lists are defined in the System.Collections.Generic namespace.
+            // You can access the list elements by their index, just like an array. (Random access)
+            // The data in a list is stored in a contiguous memory location, just like an array.
+            // The data is kept in memory after processing. 
+            // List<T> is a generic class. T is the type of elements in the list.
+            // List<T> is a dynamic array. It can grow and shrink in size.
+
+            var egypt = new Country { IsoCode = "EG", Name = "Egypt" };
+            var iraq = new Country { IsoCode = "IQ", Name = "Iraq" };
+            var jordan = new Country { IsoCode = "JO", Name = "Jordan" };
+
+            Country[] countriesArray = { egypt, iraq, jordan };
+
+
+            // ---------------------------- Constructors ----------------------------
+            List<Country> countries = new List<Country>();  // Empty list
+            //List<Country> countries2 = new List<Country>(countriesArray);  // List with initial values
+
+            List<Country> countries2 = new List<Country>(3);  // List with initial capacity
+
+
+            // ---------------------------- Properties ------------------------------
+
+            // Capacity => Gets or sets the total number of elements the internal data structure can hold without resizing.
+            // Capacity is always greater than or equal to Count. 
+            // Capacity is automatically increased when the number of elements exceeds the Capacity.
+            // Capacity is automatically decreased when the number of elements is less than a quarter of the Capacity.
+            // Capacity increases by a factor of 2 when it needs to be increased.
+            int capacity = countries.Capacity;  // 4
+
+            // Count => Gets the number of elements actually contained in the List<T>.
+            int count = countries.Count;        // 0
+
+
+
+            // ---------------------------- Methods ---------------------------------
+
+            // Add => Adds an object to the end of the List<T>.
+            countries.Add(new Country { IsoCode = "BRA", Name = "Brazil" });                // O(1)
+            Print(countries);
+
+            // AddRange => Adds the elements of the specified collection to the end of the List<T>.
+            countries.AddRange(countriesArray);                                             // O(1)
+            Print(countries);
+
+            // Insert => Inserts an element into the List<T> at the specified index.
+            countries.Insert(1, new Country { IsoCode = "SA", Name = "Saudi Arabia" });     // O(n) because it shifts the elements
+            Print(countries);
+
+            // InsertRange => Inserts the elements of a collection into the List<T> at the specified index.
+            countries.InsertRange(2, countriesArray);                                       // O(n) because it shifts the elements
+            Print(countries);
+
+            // Clear => Removes all elements from the List<T>.
+            //countries.Clear();                                                              // O(n) because it removes all elements
+
+            // Remove => Removes the first occurrence of a specific object from the List<T>.
+            //countries.Remove(egypt);      // passing the reference                    // O(n) because it shifts the elements
+            // if you want to remove by value/new object or passing a new object, you need to implement the Equals method and GetHashCode method in the Country class
+            countries.Remove(new Country { IsoCode = "EG", Name = "Egypt" });               // O(n) because it shifts the elements
+            Print(countries);
+
+            // RemoveAt => Removes the element at the specified index of the List<T>.
+            countries.RemoveAt(4);                                                           // O(n) because it shifts the elements
+            Print(countries);
+
+            // RemoveRange => Removes a range of elements from the List<T>.
+            //countries.RemoveRange(1, 2);                                                     // O(n) because it shifts the elements
+
+            // RemoveAll => Removes all the elements that match the conditions defined by the specified predicate.
+            countries.RemoveAll(c => c.IsoCode == "JO");                                     // O(n) because it shifts the elements
+            Print(countries);
+        }
+
+
+        static void Print(List<Country> countries)
+        {
+            foreach (var country in countries)
+            {
+                Console.WriteLine(country);
+            }
+
+            // ---------------------------- Properties ------------------------------
+            Console.WriteLine($"Count: {countries.Count}");                 // actual number of elements in the list 
+            Console.WriteLine($"Capacity: {countries.Capacity}");           // initial number of elements the list can hold without resizing 
+            Console.WriteLine("-----------------------------------------------------------");
+        }
+    }
+
+    class Country
+    {
+        public string IsoCode { get; set; }
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null || obj is not Country country)
+            {
+                return false;
+            }
+
+            return IsoCode.Equals(country.IsoCode, StringComparison.OrdinalIgnoreCase) && Name.Equals(country.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 7;
+            hash = hash * 13 + IsoCode.GetHashCode();
+            hash = hash * 13 + Name.GetHashCode();
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({IsoCode})";   // Egypt (EG)
+        }
+    }
+}
---
+++ 30_ListsAndDictionaries/02_Dictionary/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace _02_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Dictionary<TKey, TValue> is a collection of key-value pairs. 
+            // It is a generic collection. TKey is the type of the key, and TValue is the type of the value.
+            // Dictionary<TKey, TValue> is a hash table implementation.
+            // Dictionary<TKey, TValue> is defined in the System.Collections.Generic namespace.
+            // Dictionary<TKey, TValue> is a generic class.
+
+
+
+            var article =
+                "Dot NET is a free cross-platform and open source developer platform" +
+                "for building many different types of applications" +
+                "With Dot NET you can use multiple languages and libraries" +
+                "to build for web and IoT";
+
+            // key: 'd', value: "Dot" "Developer"
+
+            Dictionary<char, List<string>> letterDictionary = new Dictionary<char, List<string>>();
+            foreach (var word in article.Split())
+            {
+                foreach (var ch in word)
+                {
+                    var c = char.ToLower(ch);
+
+                    // ContainsKey => Determines whether the Dictionary<TKey,TValue> contains the specified key. 
+                    if (!letterDictionary.ContainsKey(c))
+                    {
+                        // Add => Adds the specified key and value to the dictionary.
+                        letterDictionary.Add(c, new List<string> { word.ToLower() });
+                    }
+
+                    // Add => Adds an element with the specified key and value into the Dictionary<TKey,TValue>.
+                    letterDictionary[c].Add(word.ToLower());
+                }
+            }
+
+            //foreach (var (key, value) in letterDictionary)
+            //{
+            //    Console.WriteLine($"key: '{key}', value: {string.Join(" ", value)}");
+            //}
+
+            foreach (var entry in letterDictionary)
+            {
+                // entry.Key is the key of the dictionary
+                Console.WriteLine($"{entry.Key}: ");
+
+                // entry.Value is the value of the dictionary
+                foreach (var value in entry.Value)
+                {
+                    Console.WriteLine($"  {value}");
+                }
+            }
+
+            Console.WriteLine("--------------------------------");
+
+            var employees = new List<Employee>
+            {
+                new Employee { Id = 1, Name = "John Doe", ReportTo = null },
+                new Employee { Id = 2, Name = "Jane Doe", ReportTo = 1 },
+                new Employee { Id = 3, Name = "Sam Doe", ReportTo = 1 },
+                new Employee { Id = 4, Name = "Don Doe", ReportTo = 2 },
+                new Employee { Id = 5, Name = "Ron Doe", ReportTo = 2 },
+                new Employee { Id = 6, Name = "Tom Doe", ReportTo = 3 },
+                new Employee { Id = 7, Name = "Tim Doe", ReportTo = 3 },
+                new Employee { Id = 8, Name = "Kim Doe", ReportTo = 3 },
+            };
+
+            //var managers = employees.GroupBy(e => e.ReportTo);
+            var managers = employees.ToLookup(e => e.ReportTo).ToDictionary(x => x.Key ?? -1, x => x.ToList());
+
+            foreach (var (key, value) in managers)
+            {
+                if (key == -1)
+                    continue;
+
+                var manager = employees.FirstOrDefault(e => e.Id == key);
+
+                Console.WriteLine($"{manager}: ");
+                foreach (var employee in value)
+                {
+                    Console.WriteLine($"  {employee}");
+                }
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ReportTo { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Name}";
+        }
+    }
+}
