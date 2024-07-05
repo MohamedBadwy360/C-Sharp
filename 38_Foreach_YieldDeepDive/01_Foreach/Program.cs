180a76d0bf1b59521be95e2974d131afaedf7fa2
Mohamed Badwy Elsaid <mohamedbadwy360@gmail.com>
2024-07-05 19:49:33 +0300

    Foreach / Yield Deep Dive

---
+++ 38_Foreach_YieldDeepDive/01_Foreach/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace _01_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Any collection in C# should implement IEnumerable interface to be able to use foreach loop on it.
+            // IEnumerable interface has only one method called GetEnumerator() which returns an IEnumerator object.
+            // IEnumerator interface has three methods: MoveNext(), Reset() and Current.
+            // MoveNext() method moves the cursor to the next element in the collection.
+            // Reset() method resets the cursor to the beginning of the collection.
+            // Current property returns the current element in the collection.
+
+            // The foreach loop is a shorthand for the following code:
+            // var enumerator = collection.GetEnumerator();
+            // while (enumerator.MoveNext())
+            // {
+            //     var item = enumerator.Current;
+            //     // Do something with item
+            // }
+
+            // The foreach loop is a read-only loop. You can't modify the collection while iterating over it.
+            // If you want to modify the collection while iterating over it, you should use for loop instead.
+
+            // The foreach loop is slower than for loop because it creates an enumerator object and calls its methods for each iteration.
+            // If you need to iterate over a collection with high performance, you should use for loop instead.
+
+            // The foreach loop is more readable and less error-prone than for loop.
+            // If you need to iterate over a collection with less code and less error, you should use foreach loop.
+
+            // The foreach loop is a syntactic sugar for the enumerator pattern.
+            // The enumerator pattern is a design pattern that allows iterating over a collection without exposing its internal structure.
+
+            // The foreach loop is a language feature that allows iterating over any collection that implements IEnumerable interface.
+            // IEnumerable interface is a part of System.Collections namespace in .NET Framework.
+
+            // Under the hood, the foreach loop uses the enumerator pattern to iterate over a collection.
+            // ClR (Common Language Runtime) calls the GetEnumerator() method on the collection to get an enumerator object.
+            // Then, it calls the MoveNext() method on the enumerator object to move the cursor to the next element in the collection.
+            // Finally, it reads the Current property of the enumerator object to get the current element in the collection.
+
+            // The foreach loop is a high-level loop that abstracts the low-level details of the enumerator pattern.
+            // It makes the code more readable and less error-prone.
+
+            // CLR doesn't understand foreach loop. It wait ntil the compiler translate the foreach loop to the enumerator pattern.
+            // The compiler translates the foreach loop to the enumerator pattern by calling the GetEnumerator() method on the collection.
+            // Then, it calls the MoveNext() method on the enumerator object to move the cursor to the next element in the collection.
+            // Finally, it reads the Current property of the enumerator object to get the current element in the collection.
+
+            // CLR understands for loop. It doesn't need the compiler to translate the for loop to the enumerator pattern.
+
+            // Recommendation: 
+            // Use foreach loop when you need to iterate over a collection with less code and less error.
+            // Use for loop when you need to iterate over a collection with high performance.
+            // Use for loop with arrays but use foreach loop with collections.
+
+
+            // ------------------------------------------------------------------------------------------- 
+
+            var numbers = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine("\n\n Using For");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($" {numbers[i]}");
+            }
+
+
+            Console.WriteLine("\n\n Using Foreach");
+            foreach (var n in numbers)
+            {
+                Console.Write($" {n}");
+            }
+
+            Console.WriteLine("\n\n Using Foreach under the hood");
+            Foreach(numbers);
+
+
+            Console.ReadKey();
+        }
+
+
+
+        // CLR do the code below for foreach loop under the hood.
+        static void Foreach<T>(IEnumerable<T> source)
+        {
+            IEnumerator<T> enumerator = source.GetEnumerator();
+            IDisposable disposable;
+
+            try
+            {
+                while (enumerator.MoveNext())
+                {
+                    var item = enumerator.Current;
+
+                    Console.Write($" {item}");
+                }
+            }
+            finally
+            {
+                disposable = (IDisposable)enumerator;
+                disposable.Dispose();
+            }
+        }
+    }
+}
---
+++ 38_Foreach_YieldDeepDive/02_Yield/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace _02_Yield
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Yield is used to return a sequence of values from a method without having to return a collection.
+            // The method that contains the yield return statement is called an iterator method.
+            // When you use yield return in a method, you indicate that the method, operator, or get accessor in which it appears is an iterator.
+            // Using yield to define an iterator removes the need for an explicit extra class to hold the state for the enumeration.
+            // The yield keyword signals to the compiler that the method in which it appears is an iterator block.
+            // The compiler generates a class to implement the behavior that is expressed in the iterator block.
+            // In the iterator block, the yield keyword is used together with return to provide a value to the enumerator object.
+            // The yield keyword is used in the block in which the value is produced, and return is used in the block from which the value is consumed.
+            // The yield statement can only appear inside an iterator block, which is a method, operator, or get accessor that contains a yield return or yield break statement.
+            // The yield statement is used to return each element one at a time.
+            // The sequence returned by an iterator method can be consumed by using a foreach statement or LINQ query.
+            // The iterator block can contain a try-catch block, but it cannot contain a try-finally block.
+            // The iterator block can contain a using statement, but it cannot contain a lock statement.
+            // The iterator block can contain a yield break statement, which will end the iteration.
+            // The iterator block can contain a yield return statement, which will return the value and continue the iteration.
+            // The iterator block can contain a yield return statement that returns a value and then exits the iterator block.
+
+
+            // yield is used to return a sequence of values from a method without having to return a collection.
+            // Yield keyword return values one by one to the caller.
+            // The caller can consume the values one by one.
+
+            // yield return: Returns a value and continues the iteration.
+            // yield break: Ends the iteration.
+
+            // yield is used very often in LINQ queries and foreach loops.
+            // yield is used very often foreach loops because foreach loops call the GetEnumerator() method of the collection and then call the MoveNext() method which in turn calls the Current property to get the value and then the loop continues.
+
+            // -----------------------------------------------------------------------------------------------
+
+            //Console.WriteLine("using GenerateV1()");
+            //foreach (var n in GenerateV1())
+            //{
+            //    Console.Write($" {n}");
+            //}
+
+            //Console.WriteLine("\n\nusing GenerateV2()");
+            //foreach (var n in GenerateV2())
+            //{
+            //    Console.Write($" {n}");
+            //}
+
+            Console.WriteLine("\n\nusing GenerateV3()");
+            foreach (var n in GenerateV3())
+            {
+                Console.Write($" {n}");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GenerateV1()
+        {
+            var result = new List<int>();
+            for (int i = 1; i <= 5; i++)
+            {
+                result.Add(i);
+            }
+            return result;
+        }
+
+        static IEnumerable<int> GenerateV2()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                // Yield return values one by one to the caller.
+                yield return i;
+            }
+        }
+
+        // Method should return IEnumerable<T> or IEnumerable / iterator block
+        static IEnumerable<int> GenerateV3()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+        }
+
+        static IEnumerable<int> GenerateV4()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+            yield break;        // End the iteration here and return control to the caller.
+            yield return 6;
+            yield return 7;
+            yield return 8;
+        }
+    }
+}
